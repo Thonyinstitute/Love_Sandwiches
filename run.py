84d33c239e9beea4b90683993259213103eb088e
Thonyinstitute <personally33@gmail.com>
2023-04-06 13:38:13 +0000

    updating sales worksheet, add new row with to the list date provided

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive.file", 
     "https://www.googleapis.com/auth/drive"
     ]
 
@@ -13,34 +13,31 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
-"""data = (SHEET.worksheet('surplus')).get_all_values()"""
-
 def get_sales_data():
     """
     Get sales figures input from the user
     """
     while True:
-        print("please enter sale data from the last market.")
+        print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
-        print("Example: 10,20,30,40,50,60\n")
+        print("Example: 10,20,30,40,50,60")
 
-
-        data_str = input("Enter your data here:")
+        data_str = input("Enter your data here: ")
 
         sales_data = data_str.split(",")
-    
-
+        
         if validate_data(sales_data):
-            print("data is valid!")
+            print("Data is valid")
             break
-
+    
+    return sales_data
 
 def validate_data(values):
     """
-    inside the try, convert all string values into integers.raises value error if string can nnot 
-    be converted into int, or if there aren't exactly 6 values.
+    Inside the try, convert all string values into integers.
+    raises valueError if string cannot be converted into int,
+    or if there arean't exactly six values.
     """
-
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -48,10 +45,20 @@ def validate_data(values):
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"invalid data: {e}, please try again. \n")
+        print(f"Invalid data: {e}, Please try again.\n")
         return False
-
     return True
 
 
-get_sales_data()
+def update_sales_worksheet(date):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
